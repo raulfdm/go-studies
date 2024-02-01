4a1aee70198c4ddc9c8af67daa2416b464e24e46
Raul de Melo <melo.raulf@gmail.com>
2024-02-01 20:21:45 +0100

    add some comments

--- File/utils/WriteJson.go
+++ File/utils/WriteJson.go
@@ -3,5 +3,7 @@ package file_utils
 import "os"
 
 func WriteJson(filename string, data string) error {
+	// 0666 is the permission
+	// []byte(data) is the content
 	return os.WriteFile(filename, []byte(data), 0666)
 }
