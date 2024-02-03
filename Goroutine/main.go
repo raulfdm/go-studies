8a2bc4b56d89f23844a5104fc1eadde0705bc9b5
Raul Melo <melo.raulf@gmail.com>
2024-02-03 15:47:25 +0100

    commit goroutine and channel example

--- Goroutine/main.go
+++ Goroutine/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	print("hey")
+	channel := make(chan string)
+
+	go doSomethingAsync(channel)
+
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
+}
+
+func doSomethingAsync(channel chan string) {
+	time.Sleep(2 * time.Second)
+	fmt.Printf("2 seconds passed\n")
+	channel <- "Done!"
+	channel <- "Really..."
+	channel <- "I mean... really..."
+	channel <- "Done!"
 }
