c6ba4dd472edadc3a3de2409cb3ed2d990d1157f
Raul Melo <melo.raulf@gmail.com>
2024-02-03 18:34:21 +0100

    add working group to execute multiple calls

--- CryptoMasters/main.go
+++ CryptoMasters/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"studies.com/cryptomasters/api"
 )
 
 func main() {
-	rate, err := api.GetRates("EUR")
+	currencies := []string{"USD", "EUR"}
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	for _, currency := range currencies {
+		wg.Add(1)
+
+		go func(currency string) {
+			defer wg.Done()
+			getRate(currency)
+		}(currency)
+	}
+
+}
+
+func getRate(currency string) {
+	rate, err := api.GetRates(currency)
 
 	if err != nil {
 		panic(err)
