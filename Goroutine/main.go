dd4c7971a2fba6879cccd48c8cb2501e090dcb1c
Raul Melo <melo.raulf@gmail.com>
2024-02-03 18:55:53 +0100

    add working group for goroutine

--- Goroutine/main.go
+++ Goroutine/main.go
@@ -2,25 +2,50 @@ package main
 
 import (
 	"fmt"
+	"sync"
+
+	// "sync"
 	"time"
 )
 
 func main() {
-	channel := make(chan string)
+	var wg sync.WaitGroup
 
-	go doSomethingAsync(channel)
+	for count := 0; count < 5; count++ {
+		wg.Add(1)
 
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
+		go func(lambdaCount int) {
+			doSomethingAsync(lambdaCount)
+			wg.Done()
+		}(count)
+	}
+
+	wg.Wait()
 }
 
-func doSomethingAsync(channel chan string) {
+func doSomethingAsync(val int) {
 	time.Sleep(2 * time.Second)
-	fmt.Printf("2 seconds passed\n")
-	channel <- "Done!"
-	channel <- "Really..."
-	channel <- "I mean... really..."
-	channel <- "Done!"
+	fmt.Printf("2 seconds passed. Val %v\n", val)
 }
+
+// Example with channels
+
+// func main() {
+// 	channel := make(chan string)
+
+// 	go doSomethingAsync(channel)
+
+// 	fmt.Println(<-channel)
+// 	fmt.Println(<-channel)
+// 	fmt.Println(<-channel)
+// 	fmt.Println(<-channel)
+// }
+
+// func doSomethingAsync(channel chan string) {
+// 	time.Sleep(2 * time.Second)
+// 	fmt.Printf("2 seconds passed\n")
+// 	channel <- "Done!"
+// 	channel <- "Really..."
+// 	channel <- "I mean... really..."
+// 	channel <- "Done!"
+// }
