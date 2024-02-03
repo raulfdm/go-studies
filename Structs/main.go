85727380e6dee82a0373bf6ba7dd701098014cdd
Raul Melo <melo.raulf@gmail.com>
2024-02-03 11:07:34 +0100

    add first struct

--- Structs/main.go
+++ Structs/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	name        string
+	yearOfBirth int
+}
+
+func (user User) CalculateAge() int {
+	return time.Now().Year() - user.yearOfBirth
+}
 
 func main() {
-	fmt.Print("structs")
+	user := User{name: "John", yearOfBirth: 1990}
+
+	fmt.Printf("User %s, born in %d, is probably %d years old", user.name, user.yearOfBirth, user.CalculateAge())
 }
