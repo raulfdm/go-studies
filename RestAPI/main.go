08899be47410bda28c6d80dcefc1aa221284d1f1
Raul de Melo <melo.raulf@gmail.com>
2024-02-05 10:08:33 +0100

    add basic server

--- RestAPI/main.go
+++ RestAPI/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	server := http.NewServeMux()
+
+	server.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "Hello World"}`))
+	})
+
+	err := http.ListenAndServe(":3000", server)
+
+	if err != nil {
+		fmt.Println("Something went wrong while trying to boot the server")
+	}
+
 }
